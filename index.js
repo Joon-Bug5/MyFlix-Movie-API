83b618eea5ea054dce8ac965ba13e1a85574463d
David <davidwonchung@gmail.com>
2022-01-13 14:00:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -81,6 +81,17 @@ app.get('/directors/:Name', passport.authenticate('jwt', {session: false}),  (re
     res.status(500).send('Error: ' + err);
   });
 });
+// Get a user by username
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 // Get all Users.
 app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.find()
